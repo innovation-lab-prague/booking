657459ef9788665654332a4f58e8451ac8c5abc3
StanSiska <stanislav.siska@gmail.com>
2018-05-31 16:28:20 +0200

    resourceId must be present to get into view

--- labRequests.js
+++ labRequests.js
@@ -3,5 +3,5 @@ var labEvents = [
   { title: 'COP Scrum Meeting / Josef P.', start: '2018-06-04',         end: '2018-06-07',          resourceId: 'B-LONGTABLE'    },
   { title: 'Risk IT Meeting / Dirk K.',  start: '2018-06-04T14:00:00',  end: '2018-06-04T15:30:00', resourceId: 'C-MEETROOM'     },
   { title: 'JV&MD FIFA 2018 / Sparta vs Slavia', start: '2018-05-31T16:30:00',  end  : '2018-05-31T16:35:00', resourceId: 'A-ROUNDTABLE' }, 
-  { title: 'Mats & Michael Farewell',    start: '2018-06-28T14:00:00',  end: '2018-06-28T18:00:00'  }
+  { title: 'Mats & Michael Farewell',    start: '2018-06-28T14:00:00',  end: '2018-06-28T18:00:00', resourceId: 'A-LONGTABLE'}
 ]
