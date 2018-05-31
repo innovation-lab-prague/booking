e802f285642a7e0fe06579334a908d0fde54338e
Stanislav Siska <stanislav.siska@gmail.com>
2018-05-31 18:01:42 +0200

    added calendar entries (#8)

--- labRequests.js
+++ labRequests.js
@@ -1,6 +1,9 @@
 var labEvents = [
   { title: 'COP Meeting / Inga M.',      start: '2018-05-30T16:30:00',  end: '2018-05-30T18:00:00', resourceId: 'A-ROUNDTABLE'   },
+  { title: 'Maintenance work', start: '2018-06-01T08:00:00',  end: '2018-06-01T12:00:00',           resourceId: 'X-MAINT'        },
   { title: 'COP Scrum Meeting / Josef P.', start: '2018-06-04',         end: '2018-06-07',          resourceId: 'B-LONGTABLE'    },
   { title: 'Risk IT Meeting / Dirk K.',  start: '2018-06-04T14:00:00',  end: '2018-06-04T15:30:00', resourceId: 'C-MEETROOM'     },
-  { title: 'Mats & Michael Farewell',    start: '2018-06-28T14:00:00',  end: '2018-06-28T18:00:00', resourceId: 'B-LONGTABLE'}
+  { title: 'HR Welcome Days / Vera S. ', start: '2018-06-11T08:00:00',  end: '2018-06-04T12:00:00', resourceId: 'B-LONGTABLE'    },
+  { title: 'M&M Farewell',               start: '2018-06-28T14:00:00',  end: '2018-06-28T18:00:00', resourceId: 'B-LONGTABLE'    }, 
+  { title: 'COP Offsite Meeting / Armin B.',  start: '2018-10-10',      end: '2018-10-12',          resourceId: 'C-MEETROOM'     }
 ]
