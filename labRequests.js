8df0779c9051ade8253f1756ae2cc7279fbd5b83
Stanislav Siska <stanislav.siska@gmail.com>
2018-05-31 18:04:16 +0200

    update description for x-maint

--- labRequests.js
+++ labRequests.js
@@ -1,6 +1,6 @@
 var labEvents = [
   { title: 'COP Meeting / Inga M.',      start: '2018-05-30T16:30:00',  end: '2018-05-30T18:00:00', resourceId: 'A-ROUNDTABLE'   },
-  { title: 'Maintenance work', start: '2018-06-01T08:00:00',  end: '2018-06-01T12:00:00',           resourceId: 'X-MAINT'        },
+  { title: 'Maintenance work / Booth',   start: '2018-06-01T08:00:00',  end: '2018-06-01T12:00:00', resourceId: 'X-MAINT'        },
   { title: 'COP Scrum Meeting / Josef P.', start: '2018-06-04',         end: '2018-06-07',          resourceId: 'B-LONGTABLE'    },
   { title: 'Risk IT Meeting / Dirk K.',  start: '2018-06-04T14:00:00',  end: '2018-06-04T15:30:00', resourceId: 'C-MEETROOM'     },
   { title: 'HR Welcome Days / Vera S. ', start: '2018-06-11T08:00:00',  end: '2018-06-04T12:00:00', resourceId: 'B-LONGTABLE'    },
