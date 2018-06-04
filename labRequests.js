046cdc0bbd9762b0a211fe410420ab9a7c41444c
Dirk Krummacker <krummacker@users.noreply.github.com>
2018-06-04 16:50:23 +0200

    Update labRequests.js (#9)
    * Update labRequests.js
    
    I would like to book the same meeting room for a f'up meeting this Wednesday.
    
    * Update labRequests.js
    
    Changed to Thursday

--- labRequests.js
+++ labRequests.js
@@ -3,6 +3,7 @@ var labEvents = [
   { title: 'Maintenance work / Booth',   start: '2018-06-01T08:00:00',  end: '2018-06-01T12:00:00', resourceId: 'X-MAINT'        },
   { title: 'COP Scrum Meeting / Josef P.', start: '2018-06-04',         end: '2018-06-07',          resourceId: 'B-LONGTABLE'    },
   { title: 'Risk IT Meeting / Dirk K.',  start: '2018-06-04T14:00:00',  end: '2018-06-04T15:30:00', resourceId: 'C-MEETROOM'     },
+  { title: 'Risk IT RDS/GUI Meeting / Dirk K.',  start: '2018-06-07T13:00:00',  end: '2018-06-07T14:30:00', resourceId: 'C-MEETROOM'     },
   { title: 'HR Welcome Days / Vera S. ', start: '2018-06-11T08:00:00',  end: '2018-06-04T12:00:00', resourceId: 'B-LONGTABLE'    },
   { title: 'M&M Farewell',               start: '2018-06-28T14:00:00',  end: '2018-06-28T18:00:00', resourceId: 'B-LONGTABLE'    }, 
   { title: 'COP Offsite Meeting / Armin B.',  start: '2018-10-10',      end: '2018-10-12',          resourceId: 'C-MEETROOM'     }
