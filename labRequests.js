c046463ae878c1fa3d5a0ee5e8edff22ef8f2f0d
AssistantsCOP <40172346+AssistantsCOP@users.noreply.github.com>
2018-06-12 09:26:25 +0200

    Update labRequests.js (#19)

--- labRequests.js
+++ labRequests.js
@@ -9,7 +9,6 @@ var labEvents = [
   { title: 'PRISMA GWT GUI Knowledge Transfer / Dirk K.',  start: '2018-06-11T14:00:00',  end: '2018-06-11T15:30:00', resourceId: 'A-ROUNDTABLE' },
   { title: 'Risk IT Meetup / Adam N.',  start: '2018-06-11T13:00:00',  end: '2018-06-11T14:00:00', resourceId: 'B-LONGTABLE' },
   { title: 'Telco / Wolfgang E.',        start: '2018-06-11T14:30:00',  end: '2018-06-11T15:00:00', resourceId: 'C-MEETROOM'     },
-  { title: 'POM Meeting/ Misa C.',       start: '2018-06-12T14:00:00',  end: '2018-06-11T15:00:00', resourceId: 'B-LONGTABLE'    },
   { title: 'FM Photoshoot / Ondrej H. ', start: '2018-06-13T09:00:00',  end: '2018-06-13T10:00:00', resourceId: 'B-LONGTABLE'    },
   { title: 'HR Welcome Days / Vera S. ', start: '2018-06-14T08:00:00',  end: '2018-06-14T12:00:00', resourceId: 'B-LONGTABLE'    },
   { title: 'POM Meeting / Misa C.',      start: '2018-06-19T14:00:00',  end: '2018-06-19T15:00:00', resourceId: 'B-LONGTABLE'    },
