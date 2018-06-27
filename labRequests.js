59fa91da78fec7674a5576ec38ae16248afb1098
Dirk Krummacker <krummacker@users.noreply.github.com>
2018-06-27 14:45:14 +0200

    Update labRequests.js
    The training is a two day training, extending one more day.

--- labRequests.js
+++ labRequests.js
@@ -15,7 +15,7 @@ var labEvents = [
   { title: 'M&M Farewell',               start: '2018-06-28T14:00:00',  end: '2018-06-28T18:00:00', resourceId: 'B-LONGTABLE'    }, 
   { title: 'IT Infrasturcture Tools Team Workshop / Josef T.',  start: '2018-07-09T13:00:00',      end: '2018-07-11T16:00:00',  resourceId: 'B-LONGTABLE' },
   { title: 'DifferenceClaimTool sprint end / Marek P.', start: '2018-07-17T10:00:00', end: '2018-07-17T17:00:00', resourceId: 'A-ROUNDTABLE'   },
-  { title: 'Risk IT Training / Dirk K.',  start: '2018-09-06', end: '2018-09-07', resourceId: 'B-LONGTABLE'},
+  { title: 'Risk IT Training / Dirk K.',  start: '2018-09-06', end: '2018-09-08', resourceId: 'B-LONGTABLE'},
   { title: 'StatistiX / Ondrej.S.',  start: '2018-06-19T11:00:00',  end: '2018-06-019T12:00:00',    resourceId: 'C-MEETROOM'     },
   { title: 'TKAMs / Sam B.',  start: '2018-06-20T14:00:00',      end: '2018-06-20T15:00:00',        resourceId: 'B-LONGTABLE'    },
   { title: 'TKAMs / Sam B.',  start: '2018-06-21T11:00:00',      end: '2018-06-21T12:00:00',        resourceId: 'B-LONGTABLE'    },
