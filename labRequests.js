8ec85f267f8a0b9fa4db71c0f72a7f7d03ac9f31
Stanislav Siska <stanislav.siska@gmail.com>
2018-10-24 11:16:44 +0200

    Bugfix

--- labRequests.js
+++ labRequests.js
@@ -105,7 +105,7 @@ var labEvents = [
   { title: 'Intercultural comm. between Eschborn/Prague and Singapore / Frank J.', start: '2018-11-06T09:00:00', end: '2018-11-06T17:00:00', resourceId: 'B-LONGTABLE' },
   { title: 'Intercultural comm. between Eschborn/Prague and Singapore / Frank J.', start: '2018-11-15T09:00:00', end: '2018-11-15T17:00:00', resourceId: 'B-LONGTABLE' },
   { title: 'Toastmasters Public Speaking / Dirk K.', start: '2018-11-15T16:00:00', end: '2018-11-15T17:00:00', resourceId: 'B-LONGTABLE' },
-  { title: 'Workshop / Lars B.', start: '2018-11-19', end: '2018-11-21', resourceId: 'B-LONGTABLE' }, 
+  { title: 'Workshop / Lars B.', start: '2018-11-19', end: '2018-11-22', resourceId: 'B-LONGTABLE' }, 
   { title: 'Board Games Event / Tomas K.', start: '2018-11-21T17:00:00', end: '2018-11-21T22:00:00', resourceId: 'B-LONGTABLE' }, 
   { title: '10th COP Anniversary / Misa C.', start: '2018-11-22T13:00:00', end: '2018-11-22T20:00:00', resourceId: 'B-LONGTABLE' },
   { title: 'Toastmasters Public Speaking / Dirk K.', start: '2018-11-29T16:00:00', end: '2018-11-29T17:00:00', resourceId: 'B-LONGTABLE' },
