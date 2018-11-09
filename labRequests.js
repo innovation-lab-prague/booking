f7a4a0f1cc2652eeb6f1764a39a066943c7525bc
waltros <43542722+waltros@users.noreply.github.com>
2018-11-09 13:39:35 +0100

    Please Update labRequests.js
    Thank you.

--- labRequests.js
+++ labRequests.js
@@ -127,6 +127,7 @@ var labEvents = [
   { title: 'Townhall Meeting / Misa C.',  start: '2018-12-05', end: '2018-12-05',     resourceId: 'B-LONGTABLE' },
   { title: 'Townhall Meeting / Misa C.',  start: '2018-12-05', end: '2018-12-05',     resourceId: 'C-MEETROOM' },
   { title: 'ExCo Lunch / Konstanze O.',  start: '2018-12-06T10:00:00', end: '2018-12-06T15:00:00',     resourceId: 'C-MEETROOM' },
+  { title: 'HR Innovation Meet-Up / Tom W.',  start: '2018-12-06T15:00:00', end: '2018-12-06T17:00:00',     resourceId: 'B-LONGTABLE' },
   { title: 'Toastmasters Public Speaking / Dirk K.', start: '2018-12-13T16:00:00', end: '2018-12-13T17:00:00', resourceId: 'B-LONGTABLE' },
   { title: 'Toastmasters Public Speaking / Dirk K.', start: '2019-01-10T16:00:00', end: '2019-01-10T17:00:00', resourceId: 'B-LONGTABLE' }
 ]
