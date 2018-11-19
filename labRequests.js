5d9ea85cafad0cc3b467edc0b72172f3a9865c2d
Stanislav Siska <stanislav.siska@gmail.com>
2018-11-19 11:20:52 +0100

    Renaming meeting

--- labRequests.js
+++ labRequests.js
@@ -125,7 +125,7 @@ var labEvents = [
   { title: 'Intercultural comm. between Eschborn/Prague and Singapore / Frank J.', start: '2018-11-15T09:00:00', end: '2018-11-15T17:00:00', resourceId: 'B-LONGTABLE' },
   { title: 'Toastmasters Public Speaking / Dirk K.', start: '2018-11-15T16:00:00', end: '2018-11-15T17:00:00', resourceId: 'B-LONGTABLE' },
   { title: 'EDT planning with / Michal K.', start: '2018-11-19T13:00:00', end: '2018-11-19T15:00:00', resourceId: 'C-MEETROOM' },
-  { title: 'Agile Community of Practice / Ondrej V', start: '2018-11-20T15:00:00', end: '2018-11-20T16:00:00', resourceId: 'C-MEETROOM' },
+  { title: 'Scrum Community of Practice / Ondrej V.', start: '2018-11-20T15:00:00', end: '2018-11-20T16:00:00', resourceId: 'C-MEETROOM' },
   { title: 'Workshop / Lars B.', start: '2018-11-19', end: '2018-11-22', resourceId: 'B-LONGTABLE' }, 
   { title: 'Board Games Event / Tomas K.', start: '2018-11-21T17:00:00', end: '2018-11-21T22:00:00', resourceId: 'B-LONGTABLE' }, 
   { title: '10th COP Anniversary / Misa C.', start: '2018-11-22T13:00:00', end: '2018-11-22T20:00:00', resourceId: 'B-LONGTABLE' },
