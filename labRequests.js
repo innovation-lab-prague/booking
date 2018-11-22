5ced99e4aaff30dfcd12f35c14b5527808c71381
Stanislav Siska <stanislav.siska@gmail.com>
2018-11-22 10:42:13 +0100

    Adding meet

--- labRequests.js
+++ labRequests.js
@@ -129,6 +129,7 @@ var labEvents = [
   { title: 'Scrum Community of Practice / Ondrej V.', start: '2018-11-20T15:00:00', end: '2018-11-20T16:00:00', resourceId: 'C-MEETROOM' },
   { title: 'StatistiX Talks / Ondrej S.',  start: '2018-11-21T14:00:00', end: '2018-11-21T15:00:00',     resourceId: 'C-MEETROOM' },
   { title: 'Board Games Event / Tomas K.', start: '2018-11-21T17:00:00', end: '2018-11-21T22:00:00', resourceId: 'B-LONGTABLE' }, 
+  { title: 'SETI Standup meet / Stan S.', start: '2018-11-22T09:00:00', end: '2018-11-22T09:30:00', resourceId: 'C-MEETROOM' },
   { title: '10th COP Anniversary / Misa C.', start: '2018-11-22T13:00:00', end: '2018-11-22T20:00:00', resourceId: 'B-LONGTABLE' },
   { title: 'RDS Knowledge Transfer / Dirk K.', start: '2018-11-23T10:00:00', end: '2018-11-23T12:00:00', resourceId: 'C-MEETROOM' },
   { title: 'XBID Product Backlog Refinement / Marek F.', start: '2018-11-26T14:00:00', end: '2018-11-26T15:00:00', resourceId: 'C-MEETROOM' },
