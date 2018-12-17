1f205d97dc08b82961c4cdb9147666b0c8af657c
Frantisek Hradil <fandah@centrum.cz>
2018-12-17 09:33:36 +0100

    RRH Job Interview (#158)

--- labRequests.js
+++ labRequests.js
@@ -160,6 +160,7 @@ var labEvents = [
   { title: 'MEGA Replacement - Design / Preuss M.', start: '2018-12-17T15:00:00', end: '2018-12-17T17:00:00', resourceId: 'C-MEETROOM' },
   { title: 'Toastmasters Public Speaking / Dirk K.', start: '2019-01-10T16:00:00', end: '2019-01-10T17:00:00', resourceId: 'B-LONGTABLE' },
   { title: 'PrismaTestTeam - BBS / Tomas A.', start: '2018-12-04T11:30:00', end: '2018-12-04T13:00:00', resourceId: 'C-MEETROOM' },
+  { title: 'RRH Job interview / Frantisek H.',  start: '2018-12-19T14:00:00', end: '2018-12-19T15:30:00', resourceId: 'C-MEETROOM' },
   { title: 'Christmas MNG Meeting / Misa C.', start: '2018-12-21T14:00:00', end: '2018-12-21T15:30:00', resourceId: 'B-LONGTABLE' },
   { title: 'Toastmasters Public Speaking / Dirk K.', start: '2019-01-24T16:00:00', end: '2019-01-24T17:00:00', resourceId: 'B-LONGTABLE' },
   { title: 'Workshop & Planning - TSA / Sebastian N.', start: '2019-02-04T12:30:00', end: '2019-02-05T19:00:00', resourceId: 'C-MEETROOM' },
