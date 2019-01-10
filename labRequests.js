c19ba74622ebd34c20aa544a0a1755f619725046
vitousek <45166860+vitousek@users.noreply.github.com>
2019-01-10 13:26:20 +0100

    Update labRequests.js

--- labRequests.js
+++ labRequests.js
@@ -1,4 +1,5 @@
 var labEvents = [
+  { title: 'COP Meeting / Ondra V.', start: '2019-01-15T14:30:00',  end: '2019-01-15T15:30:00', resourceId: 'C-MEETROOM'   },
   { title: 'HUPX Workshop / Blanka M.',      start: '2019-01-21T08:45:00',  end: '2019-01-25T18:00:00', resourceId: 'C-MEETROOM'   },
   { title: 'Reinventing Organisations / Josef P.', start: '2019-01-18T15:00:00', end: '2019-01-18T16:00:00', resourceId: 'B-LONGTABLE'    },
   { title: 'COP Meeting / Ondra V.',      start: '2018-12-04T15:00:00',  end: '2018-12-04T16:00:00', resourceId: 'C-MEETROOM'   },
