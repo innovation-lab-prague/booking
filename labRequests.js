599fe5e8bd57bcd739f2fc9e51d03a2a216b675c
Stanislav Siska <stanislav.siska@gmail.com>
2019-02-20 11:37:53 +0100

    Update labRequests.js

--- labRequests.js
+++ labRequests.js
@@ -10,6 +10,7 @@ var labEvents = [
   { title: 'M7A Cloud / Nurhayat S.', start: '2019-02-19T09:00:00', end: '2019-02-19T11:00:00', resourceId: 'C-MEETROOM'},
   { title: 'M7A Refinement / Nurhayat S.', start: '2019-01-15T12:00:00', end: '2019-01-15T13:00:00', resourceId: 'A-ROUNDTABLE'},
   { title: 'M7A Cloudification / Nurhayat S.', start: '2019-03-04T09:00:00', end: '2019-03-04T17:00:00', resourceId: 'C-MEETROOM' },
+  { title: 'Recruitment Session / Renata P.', start: '2019-03-12T14:00:00', end: '2019-03-12T16:00:00', resourceId: 'B-LONGTABLE' },
   { title: 'Marketing Workshop / Kamila L.', start: '2019-03-21T10:00:00', end: '2019-03-21T12:00:00', resourceId: 'B-LONGTABLE' },
   { title: 'COP Meeting / Ondra V.', start: '2019-01-15T14:30:00',  end: '2019-01-15T15:30:00', resourceId: 'C-MEETROOM'   },
   { title: 'HUPX Workshop / Blanka M.',      start: '2019-01-23T08:45:00',  end: '2019-01-24T18:00:00', resourceId: 'C-MEETROOM'   },
