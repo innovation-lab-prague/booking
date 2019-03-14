52d39626c3d71914caae21c244cc7c3520a7ee9e
Stanislav Siska <stanislav.siska@gmail.com>
2019-03-15 00:35:12 +0100

    Update labRequests.js
    bugfix

--- labRequests.js
+++ labRequests.js
@@ -22,7 +22,7 @@ var labEvents = [
   { title: 'Welcome Days / HR', start: '2019-04-01T09:00:00', end: '2019-04-01T12:00:00', resourceId: 'B-LONGTABLE' },
   { title: 'User Recertification Check / Elena S.', start: '2019-04-10T15:00:00', end: '2019-04-10T16:00:00', resourceId: 'B-LONGTABLE' },
   { title: 'User Recertification Check / Elena S.', start: '2019-04-11T10:00:00', end: '2019-04-11T11:00:00', resourceId: 'B-LONGTABLE' },
-  { title: 'K8s Training / David R.', start: '2019-04-08', end: '2019-04-10', resourceId: 'B-LONGTABLE' },
+  { title: 'K8s Training / David R.', start: '2019-04-08', end: '2019-04-11', resourceId: 'B-LONGTABLE' },
   { title: 'COP Meeting / Ondra V.', start: '2019-01-15T14:30:00',  end: '2019-01-15T15:30:00', resourceId: 'C-MEETROOM'   },
   { title: 'HUPX Workshop / Blanka M.',      start: '2019-01-23T08:45:00',  end: '2019-01-24T18:00:00', resourceId: 'C-MEETROOM'   },
   { title: 'Reinventing Organisations / Josef P.', start: '2019-01-18T15:00:00', end: '2019-01-18T16:00:00', resourceId: 'B-LONGTABLE'    },
