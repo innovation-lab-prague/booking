e8d29b2772dea8adaef9de89e5833cd4dc8bd1da
Stanislav Siska <stanislav.siska@gmail.com>
2019-04-16 11:30:49 +0200

    Update labRequests.js

--- labRequests.js
+++ labRequests.js
@@ -97,6 +97,7 @@ var labEvents = [
   { title: 'HR Welcome Days / Vera S. ', start: '2019-04-12T08:00:00',  end: '2019-04-12T14:00:00', resourceId: 'B-LONGTABLE'    },
   { title: 'Toastmasters Public Speaking / Dirk K.', start: '2019-04-18T16:00:00', end: '2019-04-18T17:00:00', resourceId: 'B-LONGTABLE' },
   { title: 'Scrum Training / Martin K.', start: '2019-04-24', end: '2019-04-26', resourceId: 'B-LONGTABLE' },
+  { title: 'Service Work', start: '2019-04-26T08:00:00', end: '2019-04-26T10:00:00', resourceId: 'C-MEETROOM' },
   { title: 'Asset Servicing After-work/ Ales P.',  start: '2019-04-24T17:00:00', end: '2019-04-24T19:00:00',   resourceId: 'C-MEETROOM' },  
   { title: 'Toastmasters Public Speaking / Dirk K.', start: '2019-05-02T16:00:00', end: '2019-05-02T17:00:00', resourceId: 'B-LONGTABLE' },
   { title: 'Happines At Work / Tom W.', start: '2019-05-16T10:00:00', end: '2019-05-16T11:30:00', resourceId: 'B-LONGTABLE' },
