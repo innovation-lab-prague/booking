8b4e3b70932ff357931f6e528badc04bacc95fa7
Stanislav Siska <stanislav.siska@gmail.com>
2019-06-13 10:23:26 +0200

    adding meeting per request

--- labRequests.js
+++ labRequests.js
@@ -1,5 +1,6 @@
 var labEvents = [
-  { title: 'RPA Training / Lorena N.', start: '2019-07-01T09:0:00', end: '2019-07-02T17:00:00', resourceId: 'C-MEETROOM' },
+  { title: 'STOXX Index Ops Leadership Training / Jason C.', start: '2019-08-07T09:00:00', end: '2019-08-08T18:00:00', resourceId: 'B-LONGTABLE' },
+  { title: 'RPA Training / Lorena N.', start: '2019-07-01T09:00:00', end: '2019-07-02T17:00:00', resourceId: 'C-MEETROOM' },
   { title: 'XBID Scrum Ceremonies - Review, Retrospective, Planning', start: '2019-06-13T9:30:00', end: '2019-06-13T11:00:00', resourceId: 'B-LONGTABLE' },
   { title: 'KAM Handover + Ticketing', start: '2019-06-13T10:00:00', end: '2019-06-13T15:00:00', resourceId: 'C-MEETROOM' },
   { title: 'KAM Handover + Ticketing', start: '2019-06-12T13:30:00', end: '2019-06-12T15:30:00', resourceId: 'C-MEETROOM' },
