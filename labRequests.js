7a773354e6907c359846a1e24a70a0eec8f922d8
Stanislav Siska <stanislav.siska@gmail.com>
2019-06-17 13:59:10 +0200

    Update labRequests.js

--- labRequests.js
+++ labRequests.js
@@ -177,7 +177,6 @@ var labEvents = [
   { title: 'Welcome Day / Tom W.', start: '2019-09-02T12:30:00', end: '2019-09-02T16:30:00', resourceId: 'B-LONGTABLE' },
   { title: 'Pulse Check / Ali', start: '2019-05-06T11:15:00', end: '2019-05-06T12:30:00', resourceId: 'C-MEETROOM' },
   { title: 'IFS IT Prague Team Meeting / Christian P.', start: '2019-05-22T15:00:00', end: '2019-05-22T16:30:00', resourceId: 'B-LONGTABLE' },
-  { title: 'DBP Workshop / Stan S.', start: '2019-07-03T09:00:00', end: '2019-07-04T17:00:00', resourceId: 'B-LONGTABLE' },
   { title: 'CashMIR Workshop', start: '2019-05-21T14:00:00', end: '2019-05-21T16:00:00', resourceId: 'C-MEETROOM'},
   { title: 'Farewell Event for Tomas Ch. / Stan S.', start: '2019-05-24T14:00:00', end: '2019-05-24T15:30:00', resourceId: 'B-LONGTABLE'},
   { title: 'Risk IT RDS/GUI / Dirk K.', start: '2019-06-05T15:00:00', end: '2019-06-05T16:30:00', resourceId: 'B-LONGTABLE' }
