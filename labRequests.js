f1a7cbbe1b7b0ab3bb8709740aacd695b45cd0c1
Christian Payeur <37304591+christianpayeur@users.noreply.github.com>
2019-07-02 14:46:15 +0200

    Update labRequests.js

--- labRequests.js
+++ labRequests.js
@@ -187,5 +187,6 @@ var labEvents = [
   { title: 'IFS IT Prague Team Meeting / Christian P.', start: '2019-05-22T15:00:00', end: '2019-05-22T16:30:00', resourceId: 'B-LONGTABLE' },
   { title: 'CashMIR Workshop', start: '2019-05-21T14:00:00', end: '2019-05-21T16:00:00', resourceId: 'C-MEETROOM'},
   { title: 'Farewell Event for Tomas Ch. / Stan S.', start: '2019-05-24T14:00:00', end: '2019-05-24T15:30:00', resourceId: 'B-LONGTABLE'},
-  { title: 'Risk IT RDS/GUI / Dirk K.', start: '2019-06-05T15:00:00', end: '2019-06-05T16:30:00', resourceId: 'B-LONGTABLE' }
+  { title: 'Risk IT RDS/GUI / Dirk K.', start: '2019-06-05T15:00:00', end: '2019-06-05T16:30:00', resourceId: 'B-LONGTABLE' },
+  { title: 'IFS IT Section Meeting / Christian P.', start: '2019-07-09T10:30:00', end: '2019-07-09T11:30:00', resourceId: 'B-LONGTABLE' }
 ]
