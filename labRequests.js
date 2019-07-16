13e766a7ac6e4afa4bd19c6d465d7220286d9974
vitousek <45166860+vitousek@users.noreply.github.com>
2019-07-16 09:00:06 +0200

    Update labRequests.js

--- labRequests.js
+++ labRequests.js
@@ -1,4 +1,5 @@
 var labEvents = [
+  { title: 'M7T Scrum COP  / Ondrej V.', start: '2019-07-16T14:30:00', end: '2019-07-16T15:30:00', resourceId: 'C-MEETROOM' },
   { title: 'CTS - newhire training/ Jaroslav N.', start: '2019-07-15T13:00:00', end: '2019-07-15T17:00:00', resourceId: 'C-MEETROOM' },
   { title: 'Mgmt Meeting / Kamila L.', start: '2019-09-10T14:00:00', end: '2019-09-10T15:00:00', resourceId: 'B-LONGTABLE' },
   { title: 'CorpSys Leadership / Valentina L.', start: '2019-09-03T08:00:00', end: '2019-09-03T18:00:00', resourceId: 'B-LONGTABLE' },
