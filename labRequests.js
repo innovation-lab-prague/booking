9ee00d845b986b3e968b5b70aaf49938bdd53c8e
Stanislav Siska <stanislav.siska@gmail.com>
2019-07-16 17:08:23 +0200

    Update labRequests.js

--- labRequests.js
+++ labRequests.js
@@ -1,5 +1,5 @@
 var labEvents = [
-  { title: 'Repair Works / Stan S.', start: '2018-07-19T10:00:00', end: '2018-07-19T12:00:00', resourceId: 'C-MEETROOM' },
+  { title: 'Repair Works / Stan S.', start: '2019-07-19T10:00:00', end: '2019-07-19T12:00:00', resourceId: 'C-MEETROOM' },
   { title: 'M7T Scrum COP  / Ondrej V.', start: '2019-07-30T14:30:00', end: '2019-07-30T15:30:00', resourceId: 'C-MEETROOM' },
   { title: 'Bundesbank Presentation / Eliska F.', start: '2019-07-18T09:30:00', end: '2019-07-18T12:00:00', resourceId: 'B-LONGTABLE' },
   { title: 'CTS - newhire training/ Jaroslav N.', start: '2019-07-15T13:00:00', end: '2019-07-15T17:00:00', resourceId: 'C-MEETROOM' },
