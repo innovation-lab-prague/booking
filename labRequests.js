9fdae9d79ceee61b9b10a9559bbb34b2ba72e1b3
Stanislav Siska <stanislav.siska@gmail.com>
2019-07-22 10:17:16 +0200

    Adding meet per mail request

--- labRequests.js
+++ labRequests.js
@@ -11,6 +11,7 @@ var labEvents = [
   { title: 'CorpSys Leadership / Valentina L.', start: '2019-09-04T08:00:00', end: '2019-09-04T15:00:00', resourceId: 'B-LONGTABLE' },
   { title: '<Break IT!> / Michaela', start: '2019-07-23T08:00:00',  end  : '2019-07-23T18:00:00', resourceId: 'C-MEETROOM'},
   { title: 'M7T Scrum COP  / Ondrej V.', start: '2019-07-02T14:30:00', end: '2019-07-02T15:30:00', resourceId: 'C-MEETROOM' },
+  { title: 'RPA Training / Lorena N.', start: '2019-08-27T09:00:00', end: '2019-08-29T17:00:00', resourceId: 'B-LONGTABLE' },
   { title: 'Idea Exchange / Adela P.', start: '2019-08-29T11:00:00', end: '2019-08-29T12:00:00', resourceId: 'B-LONGTABLE' },
   { title: 'Idea Exchange / Adela P.', start: '2019-07-25T11:00:00', end: '2019-07-25T12:00:00', resourceId: 'B-LONGTABLE' },
   { title: 'DBG AWS Training Day 1 / Stan S.', start: '2019-06-24T09:00:00', end: '2019-06-24T17:00:00', resourceId: 'C-MEETROOM' },
