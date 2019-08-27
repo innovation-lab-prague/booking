6c903e4711058ad363a58e0f93d40f2c9149fab3
Quot89 <Quot89@users.noreply.github.com>
2019-08-27 12:21:41 +0200

    Update labRequests.js (#304)
    * Update labRequests.js
    
    * Update labRequests.js

--- labRequests.js
+++ labRequests.js
@@ -241,6 +241,7 @@ var labEvents = [
   { title: 'Vestima Specifics / Christian P.', start: '2019-08-13T14:00:00', end: '2019-08-13T16:00:00', resourceId: 'B-LONGTABLE'},  
   { title: 'Project governance, SUMMIT, and Clearstream IT landscape / Christian P.', start: '2019-08-15T14:00:00', end: '2019-08-15T16:00:00', resourceId: 'B-LONGTABLE'},
   { title: 'Welcome Day / Tom W.', start: '2019-10-01T12:00:00', end: '2019-10-01T16:00:00', resourceId: 'B-LONGTABLE'},
+  { title: 'Auction Refinement / Pavol L.', start: '2019-08-27T13:30:00', end: '2019-08-27T14:30:00', resourceId: 'C-MEETROOM'},
   { title: 'EPEX Workshop / Veronika R.', start: '2019-09-16', end: '2019-09-17', resourceId: 'C-MEETROOM' },
   { title: 'Roadshow Digital Learning / Sven G.', start: '2019-09-23T14:00:00', end: '2019-09-23T15:00:00', resourceId: 'B-LONGTABLE'},
   { title: 'Roadshow Digital Learning / Sven G.', start: '2019-09-23T14:00:00', end: '2019-09-23T15:00:00', resourceId: 'A-ROUNDTABLE'},
