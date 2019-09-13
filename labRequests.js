ea317117d0a9e992481dc8ad9e2049218b15fe44
vitousek <45166860+vitousek@users.noreply.github.com>
2019-09-13 14:40:51 +0200

    Update labRequests.js

--- labRequests.js
+++ labRequests.js
@@ -1,4 +1,7 @@
 var labEvents = [
+  { title: 'M7T Scrum COP  / Ondrej V.', start: '2019-10-22T14:30:00', end: '2019-10-22T15:30:00', resourceId: 'C-MEETROOM' },
+  { title: 'M7T Scrum COP  / Ondrej V.', start: '2019-10-08T14:30:00', end: '2019-10-08T15:30:00', resourceId: 'C-MEETROOM' },
+  { title: 'M7T Scrum COP  / Ondrej V.', start: '2019-09-24T14:30:00', end: '2019-09-24T15:30:00', resourceId: 'C-MEETROOM' },
   { title: 'XBID - 2nd Wave Go-live / Simona H', start: '2019-11-19T08:00:00',  end  : '2019-11-19T18:00:00', resourceId: 'C-MEETROOM'},
   { title: 'VPS - TKAM Training / Jaroslav N', start: '2019-09-24T12:00:00',  end  : '2019-09-24T14:00:00', resourceId: 'C-MEETROOM'},
   { title: 'Prague Idea Exchange / Adela P.', start: '2019-10-31T11:00:00', end: '2019-10-31T12:00:00', resourceId: 'B-LONGTABLE' },
