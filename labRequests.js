7d37255b17a7c2751e1d11169c8716c7ecbfad54
Stanislav Siska <stanislav.siska@gmail.com>
2019-10-16 17:28:34 +0200

    Update labRequests.js

--- labRequests.js
+++ labRequests.js
@@ -1,5 +1,5 @@
 var labEvents = [
-  { title: 'Member section training/ Stepan Neumann', start: '2019-10-23T13:00:00', end: '2019-10-23T16:00:00', resourceId: 'C-MEETROOM' },
+  { title: 'Member section training/ Stepan N.', start: '2019-10-23T13:00:00', end: '2019-10-23T16:00:00', resourceId: 'C-MEETROOM' },
   { title: 'Risk IT Townhall / Adriana M.', start: '2019-11-29T09:00:00', end: '2019-11-29T12:00:00', resourceId: 'B-LONGTABLE' },
   { title: 'Cloud Tech Conf / Melinda R.', start: '2019-12-13T08:00:00', end: '2019-12-13T14:00:00', resourceId: 'B-LONGTABLE' },
   { title: 'Cloud Tech Conf / Melinda R.', start: '2019-12-11T08:00:00', end: '2019-12-11T19:00:00', resourceId: 'B-LONGTABLE' },
