e5318eb9054a4564097d59ffaf3ad2a07d35cb50
Stanislav Siska <stanislav.siska@deutsche-boerse.com>
2019-11-06 00:21:41 +0100

    Added per mail request

--- labRequests.js
+++ labRequests.js
@@ -1,4 +1,5 @@
 var labEvents = [
+  { title: 'Workshop / Kristina J.', start: '2019-11-06T10:30:00', end: '2019-11-06T14:00:00',resourceId: 'C-MEETROOM'},
   { title: 'Quarterly Media Checkup / Petr A.', start: '2019-11-01T15:00:00', end: '2019-11-01T15:30:00',resourceId: 'C-MEETROOM'},
   { title: 'CR+StX Dept. Workshop / Katka V.', start: '2019-11-26T10:00:00', end: '2019-11-26T12:00:00',resourceId: 'B-LONGTABLE'},
   { title: 'XBID Backlog Refinement / Jiri V.', start: '2019-10-31T10:00:00', end: '2019-10-31T11:00:00',resourceId: 'C-MEETROOM'},
