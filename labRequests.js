a86971f44f101703fd4139107da437f14dba2ac8
Christian Payeur <37304591+christianpayeur@users.noreply.github.com>
2019-11-22 13:35:23 +0100

    Update labRequests.js

--- labRequests.js
+++ labRequests.js
@@ -331,6 +331,7 @@ var labEvents = [
   { title: 'Newcomers / Huyen', start: '2019-11-05T09:30:00', end: '2019-11-05T11:30:00', resourceId: 'C-MEETROOM'},
   { title: 'AXIOMA STOXX WS / Quentin H.', start: '2019-10-31T12:00:00', end: '2019-10-31T19:00:00', resourceId: 'C-MEETROOM' },
   { title: 'Auction Refinement / Pavol L.', start: '2019-11-11T14:00:00', end: '2019-11-11T15:00:00', resourceId: 'C-MEETROOM'},
+  { title: 'IFS IT Team Info Meeting / Christian P.', start: '2019-11-25T10:30:00', end: '2019-11-25T11:30:00', resourceId: 'B-LONGTABLE'},
   { title: 'Risk Culture Workshop / Martina B.', start: '2019-11-27T11:00:00', end: '2019-11-27T12:00:00', resourceId: 'B-LONGTABLE'},
   { title: 'IFS IT Section Meeting / Christian P.', start: '2019-11-28T14:00:00', end: '2019-11-28T15:30:00', resourceId: 'B-LONGTABLE'},
   { title: 'Welcome Day / Tom W.', start: '2020-02-03T12:00:00', end: '2020-02-03T16:00:00', resourceId: 'B-LONGTABLE'},
