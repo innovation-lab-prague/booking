066295100128d40dbde4efb5483a0dcc2a78c088
Quot89 <Quot89@users.noreply.github.com>
2019-11-25 10:32:44 +0100

    Update labRequests.js (#368)

--- labRequests.js
+++ labRequests.js
@@ -332,6 +332,7 @@ var labEvents = [
   { title: 'Newcomers / Huyen', start: '2019-11-05T09:30:00', end: '2019-11-05T11:30:00', resourceId: 'C-MEETROOM'},
   { title: 'AXIOMA STOXX WS / Quentin H.', start: '2019-10-31T12:00:00', end: '2019-10-31T19:00:00', resourceId: 'C-MEETROOM' },
   { title: 'Auction Refinement / Pavol L.', start: '2019-11-11T14:00:00', end: '2019-11-11T15:00:00', resourceId: 'C-MEETROOM'},
+  { title: 'M7A Retro / Pavol L.', start: '2019-11-27T10:00:00', end: '2019-11-27T11:00:00', resourceId: 'C-MEETROOM' }, 
   { title: 'Risk Culture Workshop / Martina B.', start: '2019-11-27T11:00:00', end: '2019-11-27T12:00:00', resourceId: 'B-LONGTABLE'},
   { title: 'IFS IT Section Meeting / Christian P.', start: '2019-11-28T14:00:00', end: '2019-11-28T15:30:00', resourceId: 'B-LONGTABLE'},
   { title: 'Welcome Day / Tom W.', start: '2020-02-03T12:00:00', end: '2020-02-03T16:00:00', resourceId: 'B-LONGTABLE'},
