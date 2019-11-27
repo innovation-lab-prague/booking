8714514ebdb4971bffdd5ecb382a1e62a9b7175f
JaNiess <51701812+JaNiess@users.noreply.github.com>
2019-11-27 10:11:54 +0100

    Update labRequests.js (#374)

--- labRequests.js
+++ labRequests.js
@@ -1,5 +1,5 @@
 var labEvents = [
-  { title: 'Corporate visit / Jaroslav N.', start: '2019-11-27T09:00:00', end: '2019-11-27T18:00:00',resourceId: 'C-MEETROOM'}
+  { title: 'Corporate visit / Jaroslav N.', start: '2019-11-28T09:00:00', end: '2019-11-28T18:00:00',resourceId: 'C-MEETROOM'}
   { title: 'Trading System Analytics / Pavel S.', start: '2019-12-05T14:00:00', end: '2019-12-05T16:00:00', resourceId: 'B-LONGTABLE' },
   { title: 'XBID Demonstration for Pen. Tests / Jakub M.', start: '2019-11-26T10:00:00', end: '2019-11-26T11:30:00', resourceId: 'C-MEETROOM' },
   { title: 'HR Meeting / Adela K.', start: '2019-11-21T10:00:00', end: '2019-11-21T12:00:00', resourceId: 'C-MEETROOM' },
