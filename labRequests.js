bb1e5d3ca4ab4ec76ef3b34f1dfb7a81f0d5fd4d
Stanislav Siska <stanislav.siska@deutsche-boerse.com>
2020-01-08 15:48:23 +0100

    Added meetings per Slack request

--- labRequests.js
+++ labRequests.js
@@ -36,8 +36,10 @@ var labEvents = [
   { title: 'X-MEN Retro / Pavol L.', start: '2020-01-07T10:00:00', end: '2020-01-07T10:45:00', resourceId: 'A-ROUNDTABLE' },
   { title: 'Times Up / Lucie S.', start: '2020-01-07T17:30:00', end: '2020-01-07T21:00:00', resourceId: 'A-ROUNDTABLE' },
   { title: 'Wanderers Refinement / Pavol L.', start: '2020-01-07T14:00:00', end: '2020-01-07T15:00:00', resourceId: 'C-MEETROOM' },
-  { id: 001, title: 'HR Morning meeting / Tomas W.', start: '08:30:00', end: '10:30:00', dow: [ 4 ], resourceId: 'C-MEETROOM' },
   { title: 'Coffee Corner/allo IT stuff Prague Office / Misa C.', start: '2020-01-16T10:30:00', end: '2020-01-16T13:00:00', resourceId: 'B-LONGTABLE' },
-  { title: 'Get Together / Mina', start: '2020-01-13T14:00:00', end: '2020-01-13T15:00:00', resourceId: 'B-LONGTABLE' } 
+  { title: 'Get Together / Mina', start: '2020-01-13T14:00:00', end: '2020-01-13T15:00:00', resourceId: 'B-LONGTABLE' },
+  { title: 'IT DPT Workshop / Katerina V.', start: '2020-02-18T15:00:00', end: '2020-02-18T16:00:00', resourceId: 'B-LONGTABLE' },
+  { title: 'IT DPT Workshop / Katerina V.', start: '2020-02-19T11:00:00', end: '2020-02-19T12:30:00', resourceId: 'B-LONGTABLE' },
+  { id: 001, title: 'HR Morning meeting / Tomas W.', start: '08:30:00', end: '10:30:00', dow: [ 4 ], resourceId: 'C-MEETROOM' }
 ]
 
