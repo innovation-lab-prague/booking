91b04be7f5a41e3554b63ecf9482d35fe04fafbe
Adelainn <40064088+Adelainn@users.noreply.github.com>
2020-01-29 14:55:09 +0100

    ada fellowship infor session next Wed
    Sorry for the previous error! :)

--- labRequests.js
+++ labRequests.js
@@ -1,4 +1,5 @@
 var labEvents = [
+  { title: 'ada fellowship info-session / Adela P. & Kristyna G.', start: '2020-02-05T09:30:00', end: '2020-02-05T11:00:00', resourceId: 'B-LONGTABLE' },
   { title: 'MISP Workshop / Josef S.', start: '2020-02-27T10:00:00', end: '2020-02-27T12:00:00', resourceId: 'B-LONGTABLE' },
   { title: 'M7T OPS team setup / Ondrej V.', start: '2020-01-27T09:30:00', end: '2020-01-27T11:30:00', resourceId: 'C-MEETROOM' },
   { title: 'CERT+SOC Workshop / Josef S.', start: '2020-02-11T09:00:00', end: '2020-02-11T18:00:00', resourceId: 'B-LONGTABLE' },
