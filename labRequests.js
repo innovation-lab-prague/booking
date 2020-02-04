18ead3fb1a1ab1a00b2131d1de96a1cfb3bc6076
Stanislav Siska <stanislav.siska@deutsche-boerse.com>
2020-02-04 13:35:29 +0100

    Added meeting per request

--- labRequests.js
+++ labRequests.js
@@ -1,4 +1,5 @@
 var labEvents = [
+  { title: 'Energy Meeting / Pavel P.', start: '2020-02-13T13:00:00', end: '2020-02-13T14:00:00', resourceId: 'B-LONGTABLE' },
   { title: 'MISP Workshop / Josef S.', start: '2020-02-10T09:00:00', end: '2020-02-10T18:00:00', resourceId: 'C-MEETROOM' },
   { title: 'Mag7 retrospective / Ondrej V.', start: '2020-02-04T15:00:00', end: '2020-02-04T16:00:00', resourceId: 'C-MEETROOM' },
   { title: 'Agile CoP / Ondrej V.', start: '2020-02-12T14:00:00', end: '2020-02-12T15:00:00', resourceId: 'C-MEETROOM' },
