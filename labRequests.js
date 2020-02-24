63c2aa5d432d3bc95194eb52a724f5270b5aef93
Stanislav Siska <stanislav.siska@deutsche-boerse.com>
2020-02-24 15:05:57 +0100

    Added meeting per mail request

--- labRequests.js
+++ labRequests.js
@@ -90,5 +90,6 @@ var labEvents = [
   { title: 'IFS IT Section Meeting', start: '2020-02-21T10:30:00', end: '2020-02-21T11:00:00', resourceId: 'B-LONGTABLE'},
   { title: 'A2A Energy Project Kick-off', start: '2020-02-25T09:00:00', end: '2020-02-25T17:00:00', resourceId: 'A-ROUNDTABLE'},
   { title: 'A2A Energy Project Kick-off', start: '2020-02-25T09:00:00', end: '2020-02-25T17:00:00', resourceId: 'B-LONGTABLE'},
-  { title: 'VC Meeting / Michal F.', start: '2020-02-20T14:00:00', end: '2020-02-20T14:30:00', resourceId: 'C-MEETROOM'}
+  { title: 'VC Meeting / Michal F.', start: '2020-02-20T14:00:00', end: '2020-02-20T14:30:00', resourceId: 'C-MEETROOM'},
+  { title: 'COP Meeting / Kamila L.', start: '2020-03-30T14:00:00', end: '2020-03-30T15:00:00', resourceId: 'B-LONGTABLE'}
 ]
