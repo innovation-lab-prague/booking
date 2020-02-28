008b6b8338606942625fd3e352bc07ba49bcf67e
riccardosl <43950385+riccardosl@users.noreply.github.com>
2020-02-28 16:16:23 +0100

    CERT traning request for long table (#432)
    Two days training with external provider

--- labRequests.js
+++ labRequests.js
@@ -1,4 +1,6 @@
 var labEvents = [
+  { title: 'CERT Training / Riccardo S.',  start: '2020-03-12T09:00:00',  end : '2020-03-12T17:30:00',  resourceId: 'B-LONGTABLE'},
+  { title: 'CERT Training / Riccardo S.',  start: '2020-03-13T09:00:00',  end : '2020-03-13T17:30:00',  resourceId: 'B-LONGTABLE'},
   { title: 'Targets / Jana B.', start: '2020-03-11T13:00:00', end: '2020-03-11T17:00:00', resourceId: 'C-MEETROOM' },
   { title: 'Market Supervision/TKAM Workshop / Jan F.', start: '2020-02-26T09:00:00', end: '2020-02-26T17:00:00', resourceId: 'C-MEETROOM' },
   { title: 'Market Supervision/TKAM Workshop / Jan F.', start: '2020-02-25T09:00:00', end: '2020-02-25T17:00:00', resourceId: 'C-MEETROOM' },
