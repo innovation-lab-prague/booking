7eca33d5baf1527cefb037907ac47f4010d7205f
Jiri Vlasimsky <vlasimsky.jiri@gmail.com>
2020-03-02 08:43:10 +0100

    Update labRequests.js

--- labRequests.js
+++ labRequests.js
@@ -1,4 +1,5 @@
 var labEvents = [
+  { title: 'XBID Team Workshop',  start: '2020-03-04T14:30:00',  end : '2020-03-04T15:30:00',  resourceId: 'C-MEETROOM'},
   { title: 'CERT Training / Riccardo S.',  start: '2020-03-12T09:00:00',  end : '2020-03-12T17:30:00',  resourceId: 'B-LONGTABLE'},
   { title: 'CERT Training / Riccardo S.',  start: '2020-03-13T09:00:00',  end : '2020-03-13T17:30:00',  resourceId: 'B-LONGTABLE'},
   { title: 'Targets / Jana B.', start: '2020-03-11T13:00:00', end: '2020-03-11T17:00:00', resourceId: 'C-MEETROOM' },
